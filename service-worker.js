ef944141523d14861722fdc1aa4ae9b78ef81296
joanne-site <116017309+joanne-site@users.noreply.github.com>
2022-10-18 21:38:04 +0800

    Add files via upload
    message to show notify

--- service-worker.js
+++ service-worker.js
@@ -78,70 +78,75 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('message', function (event) {
     console.log('SW發生訊息事件:', event);
     console.log(event.data);
-    var p = caches.open(CURRENT_CACHES['post-message']).then(function (cache) {
-        switch (event.data.command) {
-            // This command returns a list of the URLs corresponding to the Request objects
-            // that serve as keys for the current cache.
-            case 'keys':
-                return cache.keys().then(function (requests) {
-                    var urls = requests.map(function (request) {
-                        return request.url;
-                    });
+    let options = {
+        body: 'push TEST',
+        icon: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+    // var p = caches.open(CURRENT_CACHES['post-message']).then(function (cache) {
+    //     switch (event.data.command) {
+    //         // This command returns a list of the URLs corresponding to the Request objects
+    //         // that serve as keys for the current cache.
+    //         case 'keys':
+    //             return cache.keys().then(function (requests) {
+    //                 var urls = requests.map(function (request) {
+    //                     return request.url;
+    //                 });
 
-                    return urls.sort();
-                }).then(function (urls) {
-                    // event.ports[0] corresponds to the MessagePort that was transferred as part of the controlled page's
-                    // call to controller.postMessage(). Therefore, event.ports[0].postMessage() will trigger the onmessage
-                    // handler from the controlled page.
-                    // It's up to you how to structure the messages that you send back; this is just one example.
-                    event.ports[0].postMessage({
-                        error: null,
-                        urls: urls
-                    });
-                });
+    //                 return urls.sort();
+    //             }).then(function (urls) {
+    //                 // event.ports[0] corresponds to the MessagePort that was transferred as part of the controlled page's
+    //                 // call to controller.postMessage(). Therefore, event.ports[0].postMessage() will trigger the onmessage
+    //                 // handler from the controlled page.
+    //                 // It's up to you how to structure the messages that you send back; this is just one example.
+    //                 event.ports[0].postMessage({
+    //                     error: null,
+    //                     urls: urls
+    //                 });
+    //             });
 
-            // This command adds a new request/response pair to the cache.
-            case 'add':
-                // If event.data.url isn't a valid URL, new Request() will throw a TypeError which will be handled
-                // by the outer .catch().
-                // Hardcode {mode: 'no-cors} since the default for new Requests constructed from strings is to require
-                // CORS, and we don't have any way of knowing whether an arbitrary URL that a user entered supports CORS.
-                var request = new Request(event.data.url, { mode: 'no-cors' });
-                return fetch(request).then(function (response) {
-                    return cache.put(event.data.url, response);
-                }).then(function () {
-                    event.ports[0].postMessage({
-                        error: null
-                    });
-                });
+    //         // This command adds a new request/response pair to the cache.
+    //         case 'add':
+    //             // If event.data.url isn't a valid URL, new Request() will throw a TypeError which will be handled
+    //             // by the outer .catch().
+    //             // Hardcode {mode: 'no-cors} since the default for new Requests constructed from strings is to require
+    //             // CORS, and we don't have any way of knowing whether an arbitrary URL that a user entered supports CORS.
+    //             var request = new Request(event.data.url, { mode: 'no-cors' });
+    //             return fetch(request).then(function (response) {
+    //                 return cache.put(event.data.url, response);
+    //             }).then(function () {
+    //                 event.ports[0].postMessage({
+    //                     error: null
+    //                 });
+    //             });
 
-            // This command removes a request/response pair from the cache (assuming it exists).
-            case 'delete':
-                return cache.delete(event.data.url).then(function (success) {
-                    event.ports[0].postMessage({
-                        error: success ? null : 'Item was not found in the cache.'
-                    });
-                });
+    //         // This command removes a request/response pair from the cache (assuming it exists).
+    //         case 'delete':
+    //             return cache.delete(event.data.url).then(function (success) {
+    //                 event.ports[0].postMessage({
+    //                     error: success ? null : 'Item was not found in the cache.'
+    //                 });
+    //             });
 
-            default:
-                // This will be handled by the outer .catch().
-                throw Error('Unknown command: ' + event.data.command);
-        }
-    }).catch(function (error) {
-        // If the promise rejects, handle it by returning a standardized error message to the controlled page.
-        console.error('Message handling failed:', error);
+    //         default:
+    //             // This will be handled by the outer .catch().
+    //             throw Error('Unknown command: ' + event.data.command);
+    //     }
+    // }).catch(function (error) {
+    //     // If the promise rejects, handle it by returning a standardized error message to the controlled page.
+    //     console.error('Message handling failed:', error);
 
-        event.ports[0].postMessage({
-            error: error.toString()
-        });
-    });
+    //     event.ports[0].postMessage({
+    //         error: error.toString()
+    //     });
+    // });
 
     // Beginning in Chrome 51, event is an ExtendableMessageEvent, which supports
     // the waitUntil() method for extending the lifetime of the event handler
     // until the promise is resolved.
-    if ('waitUntil' in event) {
-        event.waitUntil(p);
-    }
+    // if ('waitUntil' in event) {
+    //     event.waitUntil(p);
+    // }
 
     // Without support for waitUntil(), there's a chance that if the promise chain
     // takes "too long" to execute, the service worker might be automatically
@@ -164,14 +169,14 @@ self.addEventListener('notificationclick', event => {
 self.addEventListener('push', event => {
     console.log('推播訊息');
     let title = 'Server Push';
-    let options = {
-        body: 'push TEST',
-        icon: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'
-    };
     if (event.data) {
         options = event.data.json();
         title = options.title;
     }
 
+    let options = {
+        body: 'push TEST',
+        icon: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg'
+    };
     event.waitUntil(self.registration.showNotification(title, options));
 });
